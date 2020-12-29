420fca741e5c82d02551d4d2efca54fd1a1cc152
T Cai <13818704679@163.com>
2020-12-29 22:09:58 +0100

    finish HuffmanTree

--- code/DeepWalk.py
+++ code/DeepWalk.py
@@ -3,23 +3,24 @@ import numpy as np
 import torch
 from torch.nn.functional import log_softmax
 from utils.graph import Graph, read_graph
-from utils.tree import Tree
+from utils.tree import HuffmanTree
 import config
 
 
 class DeepWalk:
-    ws = 3      # window size
     wl = 5      # walk length
+    ws = 3  # window size
 
     def __init__(self, graph: Graph):
         self.G = graph
         self.N = graph.num_nodes
-        self.T = Tree(graph.nodes)
+        self.T = HuffmanTree()
+        self.T.build_from_graph(graph)
         self.D = config.D
         self.Z = np.random.rand((self.N, self.D))
 
     def sample_walk(self):
-        """ Generate a sample of random walks for each node. """
+        """Generate a sample of random walks for each node."""
         def walk(v):
             seq = [v]
             for _ in range(self.wl):
@@ -29,32 +30,25 @@ class DeepWalk:
 
         return map(walk, self.G.nodes)
 
-    def sample_context_graph(self):
+    def context_graph(self, walks):
+        """Generate a context graph from sampled walks."""
         edges = []
-        for w in self.sample_walk():
-            for i, u in enumerate(w):
+        for w in walks:
+            for i in range(len(w)):
                 j1 = max(0, i - self.ws)
                 j2 = min(len(w), i + self.ws + 1)
                 for j in range(j1, j2):
+                    # i: center node, j: context node
                     if i == j: continue
-                    v = w[j]
-                    # u: center node, v: context node
-                    edges.append([u, v])
-        random.shuffle(edges)
-        return np.array(edges)
+                    edges.append([w[i], w[j]])
+        graph = np.array(edges)
+        np.random.shuffle(graph)
+        return graph
 
     def train(self, epochs=10):
-        CG = self.sample_context_graph()
-
-    def deep_walk(self):
-        for _ in range(self.epochs):
-            walks = self.sample_walks()
-            random.shuffle(walks)
-            self.skip_gram(walks)
-
-
-class SkipGram:
-    pass
+        for epoch in range(epochs):
+            walks = self.sample_walk()
+            ctx = self.context_graph(walks)
 
 
 if __name__ == "__main__":
