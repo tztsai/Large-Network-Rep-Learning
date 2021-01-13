5e9878c7e76271959c7be621779ee704e48b0744
Zhengjie Ji <ruoji@Ruojis-MacBook-Pro.local>
2021-01-13 22:48:02 +0100

    revise lp

--- code/LinkPrediction.py
+++ code/LinkPrediction.py
@@ -11,6 +11,11 @@ from utils.txtGraphReader import txtGreader
 
 # Global                                                                          
 FRACTION_REMOVE_EDGE = 0.5
+K_TRAINING = 2 # 3
+K_TEST = 2 # 3
+DISTANCE_TYPE = 0 # 0 for common neighbors, 1 for Jaccard's coeff, 2 for AA, 3 for PA
+
+# Path settings
 # EMBEDDING_PATH = "./test/blogcatalog_NetMF_embedding_decoded.txt"
 # EMBEDDING_PATH = "./test/node2vec_blogcatalog.embed"
 # GRAPH_PATH = "./test/blogcatalogedge.txt"
@@ -26,6 +31,7 @@ class LinkPrediction():
         self.embeddings = None
         self.graph = None
         self.labels = None
+        self.data_loader = None
 
     def read_file(self, embedding_path, graph_path, label_path):
         # read embedding
@@ -39,8 +45,8 @@ class LinkPrediction():
         self.embeddings = np.array(sorted(self.embeddings, key=(lambda x:x[0]), reverse=False))
 
         # read graph
-        data_loader = txtGreader(graph_path, direct=False, weighted=True)
-        self.graph = data_loader.graph
+        self.data_loader = txtGreader(graph_path, direct=False, weighted=True)
+        self.graph = self.data_loader.graph
         self.graph = self.graph.to_undirected()
 
         # read labels
@@ -89,63 +95,150 @@ class LinkPrediction():
                 cur_e = sample(list(graph.edges), 1)[0]
                 # check if isolated
                 if graph.degree[cur_e[0]] != 1 and graph.degree[cur_e[1]] != 1:
-                    graph.remove_edge(cur_e[0], cur_e[1])#记得更新dataloader, data_loader.graph_update(graph)
+                    graph.remove_edge(cur_e[0], cur_e[1]) # update dataloader, data_loader.graph_update(graph)
                     removed_edges.append(cur_e)
                     removed = True
-
-        """
-        # add negative edges, 1 for each removed edge between random vertex pairs
+        # balance with negative edges
+        negative_edges = []
         for i in range(remove_size):
-            added = False
-            while added == False:
-                pair = sample(list(graph.nodes), 2)
-                # check if connected
-                if (pair[0], pair[1]) not in graph.edges:
-                    graph.add_edge(pair[0], pair[1])
-                    added = True
-        """
-        negative_edges = []#我觉着要不先做negative sampling
-        for i in range(remove_size):
-        
+            # sample negative edges according to weight
             flag = True
             while flag:
-                index1 = data_loader.node_sampling.draw()#目前权重是degree^0.75
-                index2 = data_loader.node_sampling.draw()
-                flag = graph.has_edge(data_loader.nodedict[index1], data_loader.nodedict[index2])
-            negative_edges.append(data_loader.nodedict[index1], data_loader.nodedict[index2])
-            
+                index1 = self.data_loader.node_sampling.draw()
+                index2 = self.data_loader.node_sampling.draw()
+                flag = graph.has_edge(self.data_loader.nodedict[index1], self.data_loader.nodedict[index2])
+            negative_edges.append((self.data_loader.nodedict[index1], self.data_loader.nodedict[index2]))
+        # assemble test split
         testsplit = []
         for item in removed_edges:
             testsplit.append([item,1])
         for item in negative_edges:
             testsplit.append([item,-1])
         
-        
         return graph, testsplit
 
     def cal_distance(self, graph):
         print(graph.edges(data=True))
+        print(graph.nodes)
+        N = len(graph.nodes)
+        dist_mat = np.zeros((N, N))
+        for i in range(N):
+            for j in range(N):
+                n1 = list(graph.nodes)[i]
+                n2 = list(graph.nodes)[j]
+                if DISTANCE_TYPE == 0:
+                    dist_mat[i][j] = self.common_neighbors(graph, n1, n2)
+                elif DISTANCE_TYPE == 1:
+                    dist_mat[i][j] = self.jaccards_coefficient(graph, n1, n2)
+                elif DISTANCE_TYPE == 2:
+                    dist_mat[i][j] = self.adamic_adar_score(graph, n1, n2)
+                else:
+                    dist_mat[i][j] = self.preferential_attachment(graph, n1, n2)
+
+        # to find the index of max value in 2*2 mat
+        print(np.where(dist_mat==np.max(dist_mat)))
+
+        # determine how many edges to predict
+
+        # while loop
+
+        # argmax to find edge in the matrix, save edge and rank value
+
+        # set max to -inf
+
+
 
 
     def get_ROC_AUC_score(self, y_true, y_score):
         return roc_auc_score(y_true, y_score)
 
-    def common_neighbors(self):
-        pass
+    def common_neighbors(self, graph, n1, n2):
+        g1 = set()
+        g2 = set()
+        for edge in graph.edges:
+            # cal neighbors
+            if n1 == edge[0]:
+                g1.add(edge[1])
+            elif n1 == edge[1]:
+                g1.add(edge[0])
 
-    def jaccards_coefficient(self):
-        pass
+            # cal neighbors
+            if n2 == edge[0]:
+                g2.add(edge[1])
+            elif n2 == edge[1]:
+                g2.add(edge[0])
 
-    def adamic_adar_score(self):
-        pass
+        return len(g1 & g2)
 
-    def preferential_attachment(self):
-        pass
+    def jaccards_coefficient(self, graph, n1, n2):
+        g1 = set()
+        g2 = set()
+        for edge in graph.edges:
+            # cal neighbors
+            if n1 == edge[0]:
+                g1.add(edge[1])
+            elif n1 == edge[1]:
+                g1.add(edge[0])
+
+            # cal neighbors
+            if n2 == edge[0]:
+                g2.add(edge[1])
+            elif n2 == edge[1]:
+                g2.add(edge[0])
+
+        return len(g1 & g2) / len(g1 | g2)
+
+    def adamic_adar_score(self, graph, n1, n2):
+        g1 = set()
+        g2 = set()
+        for edge in graph.edges:
+            # cal neighbors
+            if n1 == edge[0]:
+                g1.add(edge[1])
+            elif n1 == edge[1]:
+                g1.add(edge[0])
+
+            # cal neighbors
+            if n2 == edge[0]:
+                g2.add(edge[1])
+            elif n2 == edge[1]:
+                g2.add(edge[0])
+        # construct z
+        Z = g1 & g2
+        res = 0
+        gz = set()
+        for z in Z:
+            for edge in graph.edges:
+                # cal neighbors
+                if z == edge[0]:
+                    gz.add(edge[1])
+                elif z == edge[1]:
+                    gz.add(edge[0])
+            res += 1/np.log(len(gz))
+        return res
+
+    def preferential_attachment(self, graph, n1, n2):
+        g1 = set()
+        g2 = set()
+        for edge in graph.edges:
+            # cal neighbors
+            if n1 == edge[0]:
+                g1.add(edge[1])
+            elif n1 == edge[1]:
+                g1.add(edge[0])
+
+            # cal neighbors
+            if n2 == edge[0]:
+                g2.add(edge[1])
+            elif n2 == edge[1]:
+                g2.add(edge[0])
+
+        return len(g1) * len(g2)
 
 
 if __name__ == "__main__":
     lp = LinkPrediction()
     e, g, l = lp.read_file(EMBEDDING_PATH, GRAPH_PATH, LABEL_PATH)
-    g = lp.preprocess_graph(g)
+    g, testsplit = lp.preprocess_graph(g)
     dist = lp.cal_distance(g)
     # lp.get_ROC_AUC_score()
